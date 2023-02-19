81bb9c216a31d76e66db720b9747ef9ea3c12f35
Toni García <tgarcia@habitissimo.com>
2023-02-19 22:10:18 +0100

    styles at templating

--- thetonisite/settings.py
+++ thetonisite/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/blog/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
